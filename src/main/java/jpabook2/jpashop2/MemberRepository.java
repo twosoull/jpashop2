5c95039aeb26acc049428a00961b07777ac459be
iyeonghun <twosoull@naver.com>
2024-01-02 17:46:54 +0900

    jpa와 h2 데이터베이스 연결

--- src/main/java/jpabook2/jpashop2/MemberRepository.java
+++ src/main/java/jpabook2/jpashop2/MemberRepository.java
@@ -11,6 +11,7 @@ public class MemberRepository {
     @PersistenceContext //자동 엔티티 생성
     private EntityManager em;
 
+    //shift + command + t는 테스트코드 단축키
     public Long save(Member member){
         em.persist(member);
         return member.getId();
