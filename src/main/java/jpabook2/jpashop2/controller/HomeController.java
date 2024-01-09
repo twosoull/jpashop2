06eeb6be761ae9d004fcb293dbc880f30ff44648
iyeonghun <twosoull@naver.com>
2024-01-09 19:19:20 +0900

    회원 가입 개발

---
+++ src/main/java/jpabook2/jpashop2/controller/HomeController.java
@@ -0,0 +1,21 @@
+package jpabook2.jpashop2.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    //Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/")
+    public String home(){
+        log.info("home controller");
+
+        return "home";
+    }
+}
