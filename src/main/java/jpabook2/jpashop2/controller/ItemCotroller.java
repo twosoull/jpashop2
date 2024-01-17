cf7cbc4343aaf13e148b8338e959e9d2fa340ed0
iyeonghun <twosoull@naver.com>
2024-01-17 19:46:07 +0900

    상품 수정 로직

--- src/main/java/jpabook2/jpashop2/controller/ItemCotroller.java
+++ src/main/java/jpabook2/jpashop2/controller/ItemCotroller.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -29,9 +31,9 @@ public class ItemCotroller {
         Book book = new Book(); //세터를 다제거하고 스태틱 생성자 메소드를 만들고 사용하는게 더 좋다.
         book.setName(form.getName());
         book.setPrice(form.getPrice());
-        book.setStockQuantity(book.getStockQuantity());
-        book.setAuthor(book.getAuthor());
-        book.setIsbn(book.getIsbn());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
 
         itemService.saveItem(book);
 
@@ -45,4 +47,38 @@ public class ItemCotroller {
 
         return "items/itemList";
     }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId,Model model){
+        Book item = (Book)itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+
+        model.addAttribute("form",form);
+
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@PathVariable("itemId") Long itemId, @ModelAttribute("form") BookForm form){
+
+        Book book = new Book();
+        book.setIsbn(form.getIsbn());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setId(itemId);
+
+        itemService.saveItem(book);
+
+        return "redirect:/items";
+
+    }
+
 }
